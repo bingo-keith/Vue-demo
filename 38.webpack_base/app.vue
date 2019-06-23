793dabb44271055e8c2ef02aad52832f0463150b
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2019-06-23 17:51:21 +0800

    Vue组件化

--- 38.webpack_base/app.vue
+++ 38.webpack_base/app.vue
@@ -1,12 +1,27 @@
 <template>
-    <div>Hello {{ name }} </div>
+<div>
+    <v-title title="Vue组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
+</div>
 </template>
 
 <script>
+import vTitle from './title.vue';
+import vButton from './button.vue';
 export default {
     data() {
         return {
             name: 'Vue.js'
+        }
+    },
+    components: {
+        vTitle,
+        vButton
+    },
+    methods: {
+        handleClick(e) {
+            console.log(e);
+            
         }
     }
 }
---
+++ 38.webpack_base/title.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1 :href="'#' + title">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    }
+};
+</script>
+<style scoped>
+h1 a {
+    color: #3399ff;
+    font-size: 24px;
+}
+</style>
