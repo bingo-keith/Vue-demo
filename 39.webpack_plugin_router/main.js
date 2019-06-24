7a842d5b3c2343e8ae6fef9aaab5102c6947e4f4
hg.wb.zheng@gmail.com <hg.wb.zheng@gmail.com>
2019-06-24 20:38:49 +0800

    增加vuex

--- 39.webpack_plugin_router/main.js
+++ 39.webpack_plugin_router/main.js
@@ -1,9 +1,47 @@
 import './style.css'
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Vuex from 'vuex';
 import App from './app.vue';
 
+Vue.use(Vuex)
 Vue.use(VueRouter)
+
+const store = new Vuex.Store({
+    state: {
+        count: 0,
+        list: [1, 5, 8, 10, 30, 50]
+    },
+    mutations: {
+        increment(state, count = 1) {
+            state.count += count 
+        },
+        decrease(state, params) {
+            state.count -= params.count
+        }
+    },
+    getters: {
+        filteredList: state => {
+            return state.list.filter(item => item < 10)
+        },
+        listCount: (state, getters) => {
+            return getters.filteredList.length
+        }
+    },
+    actions: {
+        increment(context, count) {
+            context.commit('increment', count)
+        },
+        ayncIncrement(context, count) {
+            return new Promise(resolve => {
+                setTimeout(() => {
+                    context.commit('increment', 100)
+                    resolve()
+                }, 1000)
+            })
+        }
+    }
+})
 
 const Routes = [
     {
@@ -53,5 +91,6 @@ router.afterEach((to, from, next) => {
 new Vue({
     el: '#app',
     router,
+    store,
     render: h => h(App)
 })
--- 39.webpack_plugin_router/views/index.vue
+++ 39.webpack_plugin_router/views/index.vue
@@ -1,11 +1,55 @@
 <template>
     <div>
         <h1>首页</h1>
+        <p>{{$store.state.count}}</p>
+        <p>{{list}}</p>
+        <p>{{listLength}}</p>
+        <button @click="handleAsyncIncrement">async action</button>
+        <button @click="handleActionIncrement">action</button>
+        <button @click="handleIncrement(demo)">加一</button>
+        <button @click="handleDecrease(2)">减一</button>
         <router-link to="/about">跳转到 about</router-link>
     </div>
 </template>
 <script>
     export default {
-
+        data() {
+            return {
+                demo: {
+                    count: 10
+                }
+            }
+        },
+        computed: {
+            count() {
+                return this.$store.state.count + 1
+            },
+            list() {
+                return this.$store.getters.filteredList
+            },
+            listLength() {
+                return this.$store.getters.listCount
+            }
+        },
+        methods: {
+            handleIncrement(demo) {
+                this.$store.commit('increment', demo.count)
+            },
+            handleDecrease(count) {
+                // this.$store.commit('decrease', count)
+                this.$store.commit({
+                    type: 'decrease',
+                    count: count
+                })
+            },
+            handleActionIncrement() {
+                this.$store.dispatch('increment', 5)
+            },
+            handleAsyncIncrement() {
+                this.$store.dispatch('increment', 100).then(() => {
+                    console.log(this.$store.state.count)
+                })
+            }
+        }
     }
 </script>
